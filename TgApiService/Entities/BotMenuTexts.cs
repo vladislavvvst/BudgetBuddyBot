c5b66f862ca165994b3c4756faf4ea60e3c9a121
vladislavvvst <vladislavstuks@gmail.com>
2025-07-20 18:27:13 +0300

    Feature #7: добавление ограничения только владелец Feature #6: добавление кнопок управления ботом

---
+++ TgApiService/Entities/BotMenuTexts.cs
@@ -0,0 +1,12 @@
+﻿namespace TgApiService.Entities;
+
+// todo: добавить строки через resx
+internal static class BotMenuTexts
+{
+    public const string AddExpense  = "➕ Добавить трату";
+    public const string Stats       = "📊 Статистика";
+    public const string Categories  = "🗂️ Категории";
+    public const string AllExpenses = "📝 Все траты";
+    public const string Settings    = "⚙️ Настройки";
+    public const string HideMenu    = "❌ Скрыть меню";
+}
--- TgApiService/Options/TelegramOptions.cs
+++ TgApiService/Options/TelegramOptions.cs
@@ -4,4 +4,5 @@ internal class TelegramOptions
 {
     public const string Telegram = nameof(Telegram);
     public string Token { get; set; } = default!;
+    public long UserId { get; set; } = default!;
 }
